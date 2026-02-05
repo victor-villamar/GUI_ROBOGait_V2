ab5079343a671644e9b0767e4e985aa9742afc1c
victor-villamar <victorjosevillamar@gmail.com>
2026-02-05 22:41:23 +0100

    [refactor_gui] Add RoboGaitApplication class for centralized app management

--- src/main.cpp
+++ src/main.cpp
@@ -1,103 +1,20 @@
-#include <QDir>
-#include <QFile>
-#include <QFileInfo>
-#include <QGuiApplication>
-#include <QMetaType>
-#include <QQmlApplicationEngine>
-#include <QQmlContext>
-#include <QStandardPaths>
+#include <memory>
 
-#include <ament_index_cpp/get_package_share_directory.hpp>
-#include <geometry_msgs/msg/twist.hpp>
-
-#include "DataBase/DataBaseManager.hpp"
-#include "Robot/ManualControl.hpp"
-#include "Robot/RobotDiscovery.hpp"
-#include "Robot/RobotManager.hpp"
-#include "Ros/RosNodeManager.hpp"
-#include "Settings/DeveloperSettings.hpp"
-#include "User/Patient.hpp"
-#include "User/UserSession.hpp"
-
-#include <QLocale>
-#include <QTranslator>
+#include "Core/RoboGaitApplication.hpp"
 
 int main(int argc, char* argv[])
 {
-  // Virtual keyboard support
   qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
 
-  QGuiApplication app(argc, argv);
+  auto app = std::make_unique<ROBOGait::core::RoboGaitApplication>(argc, argv);
 
-  QTranslator translator;
-  const QStringList uiLanguages = QLocale::system().uiLanguages();
-  for (const QString& locale : uiLanguages)
+  app->initCommon();
+  app->initialize();
+
+  if (!app->initForNormalAppBoot())
   {
-    const QString baseName = "gui_" + QLocale(locale).name();
-    if (translator.load(":/i18n/" + baseName))
-    {
-      app.installTranslator(&translator);
-      break;
-    }
+    return -1;
   }
 
-  // ROS manager
-  ROBOGait::ros::manager::RosNodeManager rosNodeManager;
-  rosNodeManager.initialize(argc, argv);
-
-  // Database
-  auto& database = ROBOGait::db::DataBaseManager::getInstance();
-  const QString db_path_template = QString::fromStdString(ament_index_cpp::get_package_share_directory("robogait_gui")) + "/database/db_robogait.db";
-
-  const QString app_dir = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
-  QDir().mkpath(app_dir);
-
-  const QString db_runtime = app_dir + "/db_robogait.db";
-
-  const bool needs_copy = !QFile::exists(db_runtime) || QFileInfo(db_runtime).size() == 0;
-  if (needs_copy)
-  {
-    if (!QFile::exists(db_path_template))
-    {
-      qCritical() << "[main] DB template not found at:" << db_path_template;
-    }
-    else
-    {
-      QFile::remove(db_runtime);
-      if (!QFile::copy(db_path_template, db_runtime))
-      {
-        qCritical() << "[main] Failed to copy DB template from" << db_path_template << "to" << db_runtime;
-      }
-    }
-  }
-
-  database.initialize(db_runtime);
-
-  ROBOGait::session::UserSession userSession(&rosNodeManager);
-
-  // Register DeveloperSettings singleton
-  auto& developerSettings = ROBOGait::settings::DeveloperSettings::getInstance();
-  developerSettings.initializeDefaults();
-
-  qmlRegisterUncreatableType<ROBOGait::robot::discovery::RobotDiscovery>("RobotDiscovery", 1, 0, "RobotDiscovery", "Enums Only");
-  qRegisterMetaType<geometry_msgs::msg::Twist>("geometry_msgs::msg::Twist");
-
-  QQmlApplicationEngine engine;
-
-  engine.rootContext()->setContextProperty("userSession", &userSession);
-  engine.rootContext()->setContextProperty("dbManager", &database);
-  engine.rootContext()->setContextProperty("developerSettings", &developerSettings);
-
-  const QUrl url(QStringLiteral("qrc:/main.qml"));
-  QObject::connect(
-      &engine, &QQmlApplicationEngine::objectCreated, &app,
-      [url](QObject* obj, const QUrl& objUrl)
-      {
-        if (!obj && url == objUrl)
-          QCoreApplication::exit(-1);
-      },
-      Qt::QueuedConnection);
-  engine.load(url);
-
-  return app.exec();
+  return app->exec();
 }
