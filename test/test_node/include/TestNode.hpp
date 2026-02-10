09df05ee2944419d4343ae314b692e400e096ab3
victor-villamar <victorjosevillamar@gmail.com>
2026-02-10 18:39:44 +0100

    [add_test_topic_discovery] Add tests for topic-based robot discovery

---
+++ test/test_node/include/TestNode.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <rclcpp/callback_group.hpp>
+#include <rclcpp/node.hpp>
+#include <rclcpp/publisher.hpp>
+#include <rclcpp/timer.hpp>
+#include <std_msgs/msg/string.hpp>
+
+#include <chrono>
+#include <memory>
+
+#include "Ros/Define.hpp"
+
+#define T_ROBOT_STATUS "/robot_status"
+
+/**
+ * @brief Test node that publishes robot status information
+ *
+ * This node publishes robot name at 1Hz
+ */
+class TestNode
+{
+public:
+  /**
+   * @brief Constructor of TestNode class
+   *
+   * @param node Shared pointer to the parent ROS2 node
+   */
+  TestNode(std::shared_ptr<rclcpp::Node> node, const std::string& robot_namespace);
+
+  /**
+   * @brief Destructor of TestNode class
+   */
+  ~TestNode();
+
+private:
+  /**
+   * @brief Timer callback that publishes robot name
+   *
+   * This function is called at 1Hz to publish robot name.
+   */
+  void publishLoop();
+
+  std::shared_ptr<rclcpp::Node> parent_node_; /**< Parent ROS2 node */
+
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_robot_status_; /**< Publisher for robot name */
+
+  rclcpp::TimerBase::SharedPtr loop_timer_; /**< Timer for periodic publishing */
+
+  rclcpp::CallbackGroup::SharedPtr cb_group_; /**< Callback group for timer */
+
+  std::string robot_name_;      /**< Robot name */
+  std::string robot_namespace_; /**< Namespace for the node */
+};
