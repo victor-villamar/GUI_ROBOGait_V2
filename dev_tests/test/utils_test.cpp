008ae419dda5d204db08002aa3cd35a9426c375b
victor-villamar <victorjosevillamar@gmail.com>
2026-05-20 19:37:18 +0200

    [main] Delete legacy logic

--- dev_tests/test/utils_test.cpp
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-#include "../lib/utils.h"
-
-TEST(obtein_ip, EXPECT_EQ)
-{
-    EXPECT_EQ("192.168.0.2", obtenerIP()); // interiores
-    EXPECT_EQ("192.168.1.137", obtenerIP()); // exteriores
-}
